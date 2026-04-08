518d9c08617e3697b30f1f172a6340e4b5f708f3
jontseworks-oss <jontseworks@gmail.com>
2026-04-08 13:45:16 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ cartCount }) {
   return (
     <nav className="navbar">
-      <h2>StreamList</h2>
+      <h2>EZTech Stream</h2>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/movies">Movies</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/subscriptions">Store</Link></li>
+        <li><Link to="/cart">Cart ({cartCount})</Link></li>
       </ul>
     </nav>
   );
 }
 
+// THIS LINE IS REQUIRED:
 export default Navbar;
