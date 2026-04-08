aba51cde700dd8d7681c39acbefd9b48ba415a37
jontseworks-oss <jontseworks@gmail.com>
2026-04-08 13:51:08 +0800

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,7 @@
+export const subscriptions = [
+  { id: 1, name: "Premium Monthly", price: 14.99, category: "subscription" },
+  { id: 2, name: "Basic Annual", price: 99.99, category: "subscription" },
+  { id: 3, name: "EZTech T-Shirt", price: 19.99, category: "accessory" },
+  { id: 4, name: "EZTech Phone Case", price: 14.99, category: "accessory" },
+  { id: 5, name: "EZTech Hoodie", price: 39.99, category: "accessory" }
+];
